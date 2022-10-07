f575407934b91386f4afbb8951265c6ddf984a89
DaniilDDDDD <DanPanDP@mail.ru>
2022-10-07 14:22:50 +0300

    Mongock migrations initialized

--- data-service/src/main/java/web/fiiit/dataservice/DataServiceApplication.java
+++ data-service/src/main/java/web/fiiit/dataservice/DataServiceApplication.java
@@ -1,10 +1,12 @@
 package web.fiiit.dataservice;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableMongock
 public class DataServiceApplication {
 
     public static void main(String[] args) {
